0c6d8bf19df0306fc0405e580f69e896cbde4e2b
cicek <furkanncck@gmail.com>
2020-10-12 19:07:27 -0400

    Getting things donegit add .

--- src/test/java/com/review/library/step_defs/ShowRecordsStepDefs.java
+++ src/test/java/com/review/library/step_defs/ShowRecordsStepDefs.java
@@ -1,23 +1,28 @@
 package com.review.library.step_defs;
 
 import com.review.library.pages.UsersPage;
+import com.review.library.utilities.BrowserUtilities;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
 public class ShowRecordsStepDefs {
+    UsersPage usersPage = new UsersPage();
     @And("show records should have following options:")
     public void showRecordsShouldHaveFollowingOptions(List<String> options) {
         System.out.println("options.size() = " + options.size());
         System.out.println(options);
+        List<WebElement> webElements = usersPage.getShowRecords().getOptions();
+        List<String> elementsText = BrowserUtilities.getElementsText(webElements);
+        Assert.assertEquals(options,elementsText);
     }
 
     @Then("show records default value should be {int}")
     public void showRecordsDefaultValueShouldBe(Integer integer) {
         System.out.println("Selected -> "+integer);
-        UsersPage usersPage = new UsersPage();
         String actual = usersPage.getShowRecords().getFirstSelectedOption().getText();
         Assert.assertEquals(integer+"",actual);
     }
